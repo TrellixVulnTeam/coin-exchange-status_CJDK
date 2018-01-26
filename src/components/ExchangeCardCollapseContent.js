e95de3fbf8f84477379f48748d0ce3464dd2678c
Matthew Loseke <mloseke@gmail.com>
2018-01-26 10:28:59 -0600

    remove unneeded CardConent for ExchangeCardCollapsecontent

--- src/components/ExchangeCardCollapseContent.js
+++ src/components/ExchangeCardCollapseContent.js
@@ -8,19 +8,16 @@ class ExchangeCardCollapseContent extends Component {
   render() {
     let posts = this.props.posts;
     let postsSummaries = [];
+
     for (const post in posts) {
       postsSummaries.push(posts[post].details);
     }
 
-    return false
-      ? <CardContent>
-          <Typography paragraph type="body2">
-            loading ...
-          </Typography>
-        </CardContent>
-      : <CardContent>
-          {postsSummaries}
-        </CardContent>;
+    return (
+      <CardContent>
+        {postsSummaries}
+      </CardContent>
+    );
   }
 }
 
