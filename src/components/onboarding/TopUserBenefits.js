d8bb10111414c6feeaf02d4c3dcafd430eb396b5
Matthew Loseke <mloseke@gmail.com>
2018-02-12 13:18:55 -0600

    start TopUserBenefits in onboarding/

---
+++ src/components/onboarding/TopUserBenefits.js
@@ -0,0 +1,11 @@
+// @format
+
+import React, {Component} from 'react';
+
+class TopUserBenefits extends Component {
+  render() {
+    return 'top user benefits';
+  }
+}
+
+export default TopUserBenefits;
