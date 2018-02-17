04bb8c34c6f22e03b97b699c95d7ef83f01a93ef
Matthew Loseke <mloseke@gmail.com>
2018-02-17 17:08:09 -0600

    set theme for app

--- src/index.js
+++ src/index.js
@@ -4,12 +4,39 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
 import './index.css';
 import App from './App';
+import {MuiThemeProvider, createMuiTheme} from 'material-ui/styles';
 import registerServiceWorker from './registerServiceWorker';
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#ffffff',
+      main: '#f5f5f5',
+      dark: '#c2c2c2',
+      contrastText: '#000000',
+    },
+    secondary: {
+      light: '#fdff58',
+      main: '#c6ff00',
+      dark: '#90cc00',
+      contrastText: '#000000',
+    },
+    text: {
+      primary: 'rgba(0, 0, 0, 0.87)',
+      secondary: 'rgba(0, 0, 0, 0.54)',
+      disabled: 'rgba(0, 0, 0, 0.38)',
+      hint: 'rgba(0, 0, 0, 0.38)',
+      divider: 'rgba(0, 0, 0, 0.12)',
+    },
+  },
+});
+
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <MuiThemeProvider theme={theme}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </MuiThemeProvider>,
   document.getElementById('root'),
 );
 registerServiceWorker();
