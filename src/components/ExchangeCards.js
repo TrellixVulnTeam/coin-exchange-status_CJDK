d65b08244b70abfe798bcf4506c381956b6f1ca3
Matthew Loseke <mloseke@gmail.com>
2018-03-01 13:54:06 -0600

    first pass at virtualized list for displaying all exchanges

--- src/components/ExchangeCards.js
+++ src/components/ExchangeCards.js
@@ -3,12 +3,17 @@
 import React, {Component} from 'react';
 import {withStyles} from 'material-ui/styles';
 import ExchangeCard from './ExchangeCard';
+import VirtualList from 'react-tiny-virtual-list';
+import Add from './Add';
 
 const styles = theme => ({
-  container: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    justifyContent: 'flex-start',
+  virtualList: {
+    minWidth: '360px',
+    padding: `${theme.spacing.unit * 4}px ${theme.spacing.unit * 2}px 0`,
+    boxShadow: `inset 1px 0 2px ${theme.palette.text.divider}`,
+    [theme.breakpoints.down('sm')]: {
+      boxShadow: 'none',
+    },
   },
 });
 
@@ -20,14 +25,20 @@ class ExchangeCards extends Component {
     Object.entries(exchanges).forEach(array => {
       let exchange = {};
       exchange[array[0]] = array[1];
-      exchangeCards.push(
-        <ExchangeCard exchange={exchange} key={Math.random()} />,
-      );
+      exchangeCards.push(<ExchangeCard exchange={exchange} key={array[0]} />);
     });
     return (
-      <div className={classes.container}>
-        {exchangeCards}
-      </div>
+      <VirtualList
+        className={classes.virtualList}
+        width="100%"
+        height={600}
+        itemCount={exchangeCards.length}
+        itemSize={150} // Also supports variable heights (array or function getter)
+        renderItem={({index, style}) =>
+          <div key={index} style={style}>
+            {exchangeCards[index]}
+          </div>}
+      />
     );
   }
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,33 +1,13 @@
 // @format
 
 import React, {Component} from 'react';
-import {withStyles} from 'material-ui/styles';
 import ExchangeCards from './ExchangeCards';
-import Add from './Add';
-
-const styles = theme => ({
-  container: {
-    minWidth: '360px',
-    height: 'inherit',
-    overflow: 'scroll',
-    padding: `${theme.spacing.unit * 4}px ${theme.spacing.unit * 2}px 0`,
-    boxShadow: `inset 1px 0 2px ${theme.palette.text.divider}`,
-    [theme.breakpoints.down('sm')]: {
-      boxShadow: 'none',
-    },
-  },
-});
 
 class Home extends Component {
   render() {
-    let {exchanges, classes} = this.props;
-    return (
-      <div className={classes.container}>
-        <ExchangeCards exchanges={exchanges} />
-        <Add />
-      </div>
-    );
+    let {exchanges} = this.props;
+    return <ExchangeCards exchanges={exchanges} />;
   }
 }
 
-export default withStyles(styles)(Home);
+export default Home;
