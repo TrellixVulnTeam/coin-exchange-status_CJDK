8dab2b0c820efed2bc39a16d8456d36a19ef4c1d
Matthew Loseke <mloseke@gmail.com>
2018-03-07 12:39:42 -0600

    implement inifinite scroll for exchanges

--- src/components/Exchanges.js
+++ src/components/Exchanges.js
@@ -2,28 +2,78 @@
 
 import React, {Component} from 'react';
 import ExchangeCard from './ExchangeCard';
+import InfiniteScroll from 'react-infinite-scroller';
 
 class Exchanges extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      height: 0,
+      hasMore: true,
+      exchanges: [],
+      currentExchanges: [],
     };
   }
 
-  render() {
-    const exchanges = this.props.exchanges;
-    let exchangeCards = [];
-    Object.entries(exchanges).forEach(array => {
+  componentDidMount = () => {
+    /*
+     * This takes the object of objects that's passed as props.exchanges
+     * and turns them into an array of objects
+     */
+    let exchanges = [];
+    Object.entries(this.props.exchanges).forEach(array => {
       let exchange = {};
       exchange[array[0]] = array[1];
-      exchangeCards.push(<ExchangeCard exchange={exchange} key={array[0]} />);
+      exchanges.push(<ExchangeCard exchange={exchange} key={array[0]} />);
     });
-    return (
-      <div>
-        {exchangeCards}
-      </div>
-    );
+
+    /* Then we set them on state and setup the initial value of currentExchanges */
+    this.setState({exchanges}, () => {
+      const exchanges = this.state.exchanges;
+      const currentExchanges = exchanges.slice(0, 10);
+      this.setState({currentExchanges});
+    });
+  };
+
+  loadMore = page => {
+    const numberOfExchangesForThePage = this.numberOfExchangesFor(page);
+    const exchanges = this.state.exchanges;
+    const currentExchanges = exchanges.slice(0, numberOfExchangesForThePage);
+    this.setState({currentExchanges}, () => {
+      this.state.currentExchanges.length === this.state.exchanges.length
+        ? this.setState({hasMore: false})
+        : this.setState({hasMore: true});
+    });
+  };
+
+  numberOfExchangesFor = page => {
+    const numberOfExchangesPerPage = 10;
+    switch (page) {
+      case 0:
+        return numberOfExchangesPerPage;
+      default:
+        return numberOfExchangesPerPage * page + 10;
+    }
+  };
+
+  render() {
+    let currentExchanges = this.state.currentExchanges;
+    return currentExchanges
+      ? <div style={{height: '100%', overflow: 'auto', padding: '32px 16px'}}>
+          <InfiniteScroll
+            page={0}
+            hasMore={this.state.hasMore}
+            initialLoad={false}
+            loadMore={this.loadMore}
+            loader={
+              <div className="loader" key={Math.random}>
+                loading...
+              </div>
+            }
+            useWindow={false}>
+            {currentExchanges}
+          </InfiniteScroll>
+        </div>
+      : null;
   }
 }
 
