400c845c7a2e41db7cb0fd7245cd644d9a2bf6e2
Matthew Loseke <mloseke@gmail.com>
2018-03-14 19:40:53 -0500

    removes PropTypes import from NoResults component

--- src/components/NoResults.js
+++ src/components/NoResults.js
@@ -1,7 +1,6 @@
 // @format
 
 import React, {Component} from 'react';
-import PropTypes from 'prop-types';
 import Card, {CardContent} from 'material-ui/Card';
 import Typography from 'material-ui/Typography';
 
