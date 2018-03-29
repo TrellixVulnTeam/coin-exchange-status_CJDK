6b3263fb00a1b800cb0ce7b21130c5e3cca8a1e9
Matthew Loseke <mloseke@gmail.com>
2018-03-29 13:26:03 -0500

    fix logic around getPageFrom's determination of wether it's on the home page or onboarding page (see comments in file)

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -6,9 +6,18 @@ import localStorage from './localStorage';
 const getPageFrom = pathname => {
   switch (pathname) {
     case '/':
-      return localStorage.getItem(isFirstRun) === 'true'
-        ? '/onboarding/top-user-benefits'
-        : '/home';
+      /*
+       * If they've NEVER been to the site, getItem will return null.
+       * If they've been to the site and gotten past onboarding's top user 
+       * benefits screen, then getItem should return 'false' (a string).
+       * The only time it would be 'true' (a string) is if after they've
+       * initially gotten past onboarding and started using the app, and then
+       * went into settings and toggled the switch to show onboarding's top
+       * user benefits.
+       */
+      return localStorage.getItem(isFirstRun) === 'false'
+        ? 'home'
+        : '/onboarding/top-user-benefits';
     default:
       return pathname;
   }
